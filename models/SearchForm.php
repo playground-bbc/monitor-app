bc7832b4c20b3967d98a98bc925305b46f178fa7
Eduardo Morales <spiderbbc@gmail.com>
2019-05-21 13:36:23 -0400

    adding form model

---
+++ models/SearchForm.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+
+/**
+ * SearchForm is the model behind the search form.
+ */
+class SearchForm extends Model
+{
+    public $text_search;
+    public $query_search = [
+        'and' => '&',
+        'or' => '<>'
+    ];
+    public $keywords = [];
+    public $negative_words = [
+        'malo',
+        'terrible'
+    ];
+    public $start_date;
+    public $end_date;
+
+
+    /**
+     * @return array the validation rules.
+     */
+    public function rules()
+    {
+        return [
+            // text_search  are required
+            [['text_search'], 'required'],
+            // text_search has to be a valid string
+            [['text_search'], 'string'],
+            // start date needs to be entered correctly
+            [['start_date','end_date'], 'date'],
+        ];
+    }
+
+    /**
+     * @return array customized attribute labels
+     */
+    public function attributeLabels()
+    {
+        return [
+            'text_search' => 'texto de Busqueda',
+            'start_date' => 'Fecha de Inicio',
+            'start_end' => 'Fecha Final',
+            'keywords' => 'Palabras Claves',
+        ];
+    }
+
+    /**
+     * Sends an email to the specified email address using the information collected by this model.
+     * @param string $email the target email address
+     * @return bool whether the model passes validation
+    
+    public function contact($email)
+    {
+        if ($this->validate()) {
+            Yii::$app->mailer->compose()
+                ->setTo($email)
+                ->setFrom([Yii::$app->params['senderEmail'] => Yii::$app->params['senderName']])
+                ->setReplyTo([$this->email => $this->name])
+                ->setSubject($this->subject)
+                ->setTextBody($this->body)
+                ->send();
+
+            return true;
+        }
+        return false;
+    }
+     */
+}
