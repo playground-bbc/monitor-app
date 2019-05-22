0549e09dcf5828d4b7f252cc334389c4356ae5fc
Eduardo Xavier <eduardo@montana-studio.com>
2019-05-22 09:22:54 -0600

    view twitter

--- modules/monitor/views/twitter/index.php
+++ modules/monitor/views/twitter/index.php
@@ -28,7 +28,7 @@ $moduleName = $this->context->action->id;
 
         <div class="row">
             <div class="col-lg-12">
-                <?= Html::beginForm(Url::to('index'),'post',['class' => 'form-group']); ?>
+                <?= Html::beginForm(Url::to(['index']),'post',['class' => 'form-group']); ?>
                 <?= Html::input('search', 'search_twitter','',['class' => 'form-control']); ?>
                 <div class="form-group" style="padding-top: 10px ">
                     <?= Html::submitButton('Buscar', ['class' => 'btn btn-primary']); ?>
