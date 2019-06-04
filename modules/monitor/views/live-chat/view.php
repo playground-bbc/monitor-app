6462f0de40469685b6790c198e65bfdfc44eb51f
Eduardo Xavier <eduardo@montana-studio.com>
2019-06-03 18:47:54 -0600

    working with table

--- modules/monitor/views/live-chat/view.php
+++ modules/monitor/views/live-chat/view.php
@@ -33,16 +33,45 @@ $abbreviation_name = ArrayHelper::map($productsCategories,'id','abbreviation_nam
               <tbody>
                 <?php $index = 0; foreach ($productsCategories as $key => $value): ?>
                         <tr>
-                            <td rowspan="<?= $index *= 4 ?>"><?= $value->abbreviation_name  ?></td>
+                            <td rowspan="<?= 1 ?>"><?= $value->abbreviation_name  ?></td>
                             <?php foreach ($value->products as $key => $products) :?>
-                                <td><?= $products->name  ?></td>
+                                <td rowspan="<?= 1 ?>"><?= $products->name  ?></td>
                                 <?php foreach ($products->models as $key => $model) :?>
-                                    <td><?= $model->name  ?></td>
+                                    <td rowspan="<?= 2 ?>"><?= $model->name  ?></td>
                                 <?php endforeach; ?>    
                             <?php endforeach; ?>  
                         </tr> 
                 <?php endforeach; ?>
                 
+              </tbody>
+            </table>
+          </div>
+    </div>
+
+
+    <div class="bs-docs-example">
+            <table class="table table-bordered">
+              <thead>
+                <tr>
+                  <th>Categoria</th>
+                  <th>Productos</th>
+                  <th>Modelos</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php foreach ($productsCategories as $key => $category): ?>
+                    <tr>
+                      <td rowspan=""><?= $category->abbreviation_name  ?></td>
+                        <td>
+                            <ul>
+                            <?php foreach ($category->products as $key => $products) :?>
+                                <li><?= $products->name  ?></li>
+                            <?php endforeach;  ?>
+                            </ul> 
+                        </td>
+                      <td>Mark</td>
+                    </tr>
+                <?php endforeach;  ?>
               </tbody>
             </table>
           </div>
