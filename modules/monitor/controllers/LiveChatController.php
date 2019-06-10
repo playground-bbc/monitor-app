e3411abd295eeb406e6b592f2b26cf51490f951c
Eduardo Xavier <eduardo@montana-studio.com>
2019-06-10 13:11:49 -0600

    solved errors

--- modules/monitor/controllers/LiveChatController.php
+++ modules/monitor/controllers/LiveChatController.php
@@ -17,55 +17,18 @@ class LiveChatController extends Controller
      * Renders the index view for the module
      * @return string
      */
-    public function actionView()
+    public function actionCreate()
     {
         $form_model           = new SearchForm();
         $form_model->scenario = 'live-chat';
 
-        $params = [
-            'date_from' => '2018-06-05',
-            'date_to'   => '2019-06-06',
-            //  'query' => "50UK6300PSB"
-        ];
-        $liveChat = new LiveChatApi();
-        $liveChat->setParams($params);
+        $this->render('create',['form_model' => $form_model])
 
-        $products_models    = ProductsModels::find()->asArray()->all();
-        $dictionary         = new Dictionary;
-        $productsCategories = ProductsCategories::find()->where(['or', ['parentId' => 0], ['parentId' => null]])->all();
-
-        $pages = $liveChat->chatByQuery($products_models, 1);
-
-        // var_dump($liveChat->getData());
-        /*$count_words = $liveChat->searchAndCountBywords($dictionary->orderedwords,1);
-    $words_per_product_line = $liveChat->add_words_per_product_line($count_words);
-    $total_tickets['total'] = $liveChat->get_total_tickets(1);
-    $get_tickets_number_of_tickets_status = $liveChat->get_tickets_number_of_tickets_status(1);
-
-    $total_tickets = ArrayHelper::merge($total_tickets,$get_tickets_number_of_tickets_status);
-
-    return $this->render('view',[
-    'productsCategories' => $productsCategories,
-    'words_per_product_line' => $words_per_product_line,
-    'count_words' => $count_words,
-    'total_tickets' => $total_tickets,
-    ]);*/
     }
 
     public function actionDefault()
     {
-        $model  =  new LiveChatApi();
-        $params = [
-            'date_from' => '2018-06-05',
-            'date_to'   => '2019-06-06',
-            'query'     => ["50UK6300PSB","50UK6300PSB"],
-        ];
-
-        $model->setParams($params);
-        $property = $model->getParams($params);
-        echo "<pre>";
-        var_dump($model->build());
-        echo "</pre>";
+        
     }
 
 }
