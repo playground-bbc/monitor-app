330d7f9f431cf5ba3044d8ffb0a9eaa84f1d6ec6
Eduardo morales <spiderbbc@gmail.com>
2019-07-28 21:36:31 -0400

    change show view

--- modules/monitor/views/alert/show.php
+++ modules/monitor/views/alert/show.php
@@ -11,46 +11,75 @@ use yii\data\ArrayDataProvider;
  		</div>
  	</div>
  	
- 	<!-- detail alert </!-->
-	<?php if ($alert): ?>
-		<?=$this->render('tables/_detailAlert',['alert' => $alert]); ?>
- 	<?php endif ?>
- 	<!-- detail alert </!-->
- 	<!-- count categories tweet </!-->
-	<?php if ($chartCategories->getCategories('countByCategoryInTweet')): ?>
-		<?=$this->render('charts/_countByCategoryInTweet',['chartCategories' => $chartCategories]); ?>
- 	<?php endif ?>
- 	<!-- count words tweet </!-->
- 	<?php if ($chartWords->getSeries('countWords')): ?>
-		<?=$this->render('charts/_countWordsTweet',['chartWords' => $chartWords]); ?>
- 	<?php endif ?>
- 	<!-- count sentences tweet </!-->
- 	<?php if (isset($model['tweets'])): ?>
- 		<?php if (isset($model['tweets']['sentences'])): ?>
-			<?=$this->render('tables/_sentencesTweet',['sentences' => $model['tweets']['sentences'],'alertId' => $alert->id]); ?>
- 		<?php endif ?>
- 	<?php endif ?>
-
- 	<!-- Live chat countByCategory </!-->
- 	<?php if ($chartCategories->getCategories('countByCategoryInLiveChat')): ?>
-		<?=$this->render('charts/_countByCategoryInLive',['chartCategories' => $chartCategories]); ?>
- 	<?php endif ?>
- 	<!-- count words live </!-->
- 	<?php if ($chartWords->getSeries('countWords_live')): ?>
-		<?=$this->render('charts/_countWordsLive',['chartWords' => $chartWords]); ?>
- 	<?php endif ?>
- 	<!-- count sentences Live </!-->
- 	<?php if (isset($model['liveChat'])): ?>
- 		<?php if (isset($model['liveChat']['sentences_live'])): ?>
-			<?=$this->render('tables/_sentencesLive',['sentences' => $model['liveChat']['sentences_live'],'alertId' => $alert->id]); ?>
- 		<?php endif ?>
- 	<?php endif ?>
- 	<!-- total ticket Live </!-->
- 	<?php if (isset($model['liveChat'])): ?>
- 		<?php if (isset($model['liveChat']['total'])): ?>
-			<?=$this->render('charts/_totalTicektLive',['chartLive' => $chartLive]) ?>
- 		<?php endif ?>
- 	<?php endif ?>
+ 	<div class="container">
+ 		<!-- detail alert </!-->
+		<?php if ($alert): ?>
+			<?=$this->render('tables/_detailAlert',['alert' => $alert]); ?>
+	 	<?php endif ?>
+	 	<!-- detail alert </!-->
+	 	<div class = "row">
+	 		<div class="col-md-6">
+	 			<!-- count categories tweet </!-->
+				<?php if ($chartCategories->getCategories('countByCategoryInTweet')): ?>
+					<?=$this->render('charts/_countByCategoryInTweet',['chartCategories' => $chartCategories]); ?>
+			 	<?php endif ?>
+	 		</div>
+	 		<div class="col-md-6">
+				<!-- count words tweet </!-->
+			 	<?php if ($chartWords->getSeries('countWords')): ?>
+					<?=$this->render('charts/_countWordsTweet',['chartWords' => $chartWords]); ?>
+			 	<?php endif ?> 			
+	 		</div>
+	 	</div>
+	 	<div class="row">
+	 		<div class="col-md-12">
+	 			<!-- count sentences tweet </!-->
+			 	<?php if (isset($model['tweets'])): ?>
+			 		<?php if (isset($model['tweets']['sentences'])): ?>
+						<?=$this->render('tables/_sentencesTweet',['sentences' => $model['tweets']['sentences'],'alertId' => $alert->id]); ?>
+			 		<?php endif ?>
+			 	<?php endif ?>
+	 		</div>
+	 	</div>
+ 	</div>
+ 	<hr>
+ 	<div class="row">
+ 		<div class="col-md-12">
+			<!-- Live chat countByCategory </!-->
+		 	<?php if ($chartCategories->getCategories('countByCategoryInLiveChat')): ?>
+				<?=$this->render('charts/_countByCategoryInLive',['chartCategories' => $chartCategories]); ?>
+		 	<?php endif ?>
+ 		</div>
+ 	</div>
+	<div class="row">
+		<div class="col-md-6">
+			<!-- count words live </!-->
+		 	<?php if ($chartWords->getSeries('countWords_live')): ?>
+				<?=$this->render('charts/_countWordsLive',['chartWords' => $chartWords]); ?>
+		 	<?php endif ?>
+		</div>
+		<div class="col-md-6">
+			<!-- total ticket Live </!-->
+		 	<?php if (isset($model['liveChat'])): ?>
+		 		<?php if (isset($model['liveChat']['total'])): ?>
+					<?=$this->render('charts/_totalTicektLive',['chartLive' => $chartLive]) ?>
+		 		<?php endif ?>
+		 	<?php endif ?>
+		</div>
+	</div>
+ 	<div class="row">
+ 		<div class="col-md-12">
+ 			<!-- count sentences Live </!-->
+		 	<?php if (isset($model['liveChat'])): ?>
+		 		<?php if (isset($model['liveChat']['sentences_live'])): ?>
+					<?=$this->render('tables/_sentencesLive',['sentences' => $model['liveChat']['sentences_live'],'alertId' => $alert->id]); ?>
+		 		<?php endif ?>
+		 	<?php endif ?>
+ 		</div>
+ 	</div>
+ 	
+ 	<hr>
+ 	
 
  	
  	<!-- total awario categories </!-->
